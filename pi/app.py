b1cc24fc43a5ccd0db6ba2a4d3dbf6ca2c938c8a
pbnsilva <pedrobnsilva@gmail.com>
2016-05-05 16:17:55 +0200

    adds offsets

--- pi/app.py
+++ pi/app.py
@@ -7,6 +7,14 @@ mx = Multiplex(1)
 
 sensors = [2, 3, 4, 5]
 
+# acc x, y, z, gyro x, y, z
+offsets = {
+    2: [2563, 2128, 1306, 85, -18, 22],
+    3: [-1523, -276, 1773, 60, -13, -3],
+    4: [-3131, 1430, 1332, 72, 10, 26],
+    5: [-1944, -823, 1969, 121, -11, 20]
+}
+
 mpus = {}
 
 # initialize
@@ -14,6 +22,12 @@ for i in sensors:
     mpus[i] = mpu6050.MPU6050()
     mpus[i].dmpInitialize()
     mpus[i].setDMPEnabled(True)
+    mpus[i].setXAccelOffset(offsets[i][0])
+    mpus[i].setYAccelOffset(offsets[i][1])
+    mpus[i].setZAccelOffset(offsets[i][2])
+    mpus[i].setXGyroOffset(offsets[i][3])
+    mpus[i].setYGyroOffset(offsets[i][4])
+    mpus[i].setZGyroOffset(offsets[i][5])
     packetSize = mpus[i].dmpGetFIFOPacketSize()
 
 
@@ -38,7 +52,7 @@ while True:
         ypr = mpu.dmpGetYawPitchRoll(q, g)
 
         print i, q['x'], q['y'], q['z'], q['w']
-        
+
         #print(ypr['yaw'] * 180 / math.pi),
         #print(ypr['pitch'] * 180 / math.pi),
         #print(ypr['roll'] * 180 / math.pi)
@@ -46,6 +60,3 @@ while True:
         # track FIFO count here in case there is > 1 packet available
         # (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize
-
-
-
